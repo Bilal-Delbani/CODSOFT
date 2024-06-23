db4ea86e97f5d2fe4ecda844e0dbea5da1c07149
Bilal Delbani <bibodado313@gmail.com>
2024-06-23 09:53:07 +0300

    this class is added to remove the course textfield and the grade textfield and both the add and remove buttons of the corresping row that the remove button belongs to it, so I used to save these components as objects to keep track each button and its components with their index

---
+++ StudentGradeCalculator/src/Control/RemoveButtonListener.java
@@ -0,0 +1,40 @@
+package Control;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+public class RemoveButtonListener implements ActionListener {
+    private int index;
+    private HashMap<Integer, JTextField> courseHash = new HashMap<>();
+    private HashMap<Integer, JTextField> gradeHash = new HashMap<>();
+    private HashMap<Integer, JButton> addHash = new HashMap<>();
+    private HashMap<Integer, JButton> removeHash = new HashMap<>();
+    private JPanel addPanel;
+
+    public RemoveButtonListener(HashMap<Integer, JTextField> courseHash, HashMap<Integer, JTextField> gradeHash, HashMap<Integer, JButton> addHash, HashMap<Integer, JButton> removeHash, int GridY, JPanel addPanel){
+        index = GridY;
+        this.courseHash = courseHash;
+        this.gradeHash = gradeHash;
+        this.addHash = addHash;
+        this.removeHash = removeHash;
+        this.addPanel = addPanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JTextField course = courseHash.remove(index);
+        JTextField grade = gradeHash.remove(index);
+        JButton add = addHash.remove(index);
+        JButton remove = removeHash.remove(index);
+
+        addPanel.remove(course);
+        addPanel.remove(grade);
+        addPanel.remove(add);
+        addPanel.remove(remove);
+
+        addPanel.revalidate();
+        addPanel.repaint();
+    }
+}
